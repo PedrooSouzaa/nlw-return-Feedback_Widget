7402d9f107b4ccec37f7f700b0d3cea6ece5be02
Pedro Souza <pedrossouza00@gmail.com>
2022-05-08 17:36:19 -0300

    email arrumado

--- web/src/components/WidgetForm/ScreenshotButton.tsx
+++ web/src/components/WidgetForm/ScreenshotButton.tsx
@@ -18,7 +18,7 @@ export function ScreenshotButton({screenshot, onScreenshotTook}: ScreenshotButto
     const canvas = await html2canvas(document.querySelector('html')!);
     const base64image = canvas.toDataURL('image/png');
 
-    onScreenshotTook("data:image/png;base64");
+    onScreenshotTook(base64image);
     setIsTakingScreenshot(false);
   }
 
